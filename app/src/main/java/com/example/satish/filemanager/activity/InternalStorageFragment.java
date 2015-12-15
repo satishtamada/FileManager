bd02b8ed210ac59b5ed863c5f744244eefc96e44
satishtamada <satish@confianza.co.in>
2015-12-15 15:02:38 +0530

    conflits resolve

--- app/src/main/java/com/example/satish/filemanager/activity/InternalStorageFragment.java
+++ app/src/main/java/com/example/satish/filemanager/activity/InternalStorageFragment.java
@@ -47,11 +47,6 @@ public class InternalStorageFragment extends Fragment implements InternalStorage
     private String selectedFolderName;
     private int selectedFilePosition;
     private List<String> selectedFilePositions = new ArrayList<String>();
-<<<<<<< Updated upstream
-
-
-=======
->>>>>>> Stashed changes
     //creating conflicts for child
     public InternalStorageFragment() {
         // Required empty public constructor
