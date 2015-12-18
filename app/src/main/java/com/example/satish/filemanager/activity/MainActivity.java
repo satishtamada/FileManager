20e468874cc7fd5cdfcf41fe14058b3248635983
satishtamada <satish@confianza.co.in>
2015-12-18 09:42:24 +0530

    file updated'

--- app/src/main/java/com/example/satish/filemanager/activity/MainActivity.java
+++ app/src/main/java/com/example/satish/filemanager/activity/MainActivity.java
@@ -9,27 +9,18 @@ import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
-import android.widget.ListView;
-
 import com.example.satish.filemanager.R;
-import com.example.satish.filemanager.adapter.InternalStorageFilesAdapter;
-import com.example.satish.filemanager.model.InternalStorageFilesModel;
 
-import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity implements FragmentDrawer.FragmentDrawerListener {
     private Toolbar mToolbar;
     private FragmentDrawer drawerFragment;
-    private ListView listView;
-    private ArrayList<InternalStorageFilesModel> filesModelArrayList;
-    private InternalStorageFilesAdapter internalStorageFilesAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         mToolbar = (Toolbar) findViewById(R.id.toolbar);
-        listView = (ListView) findViewById(R.id.listview);
         //set toolbar
         setSupportActionBar(mToolbar);
         getSupportActionBar().setDisplayShowHomeEnabled(true);
@@ -37,6 +28,7 @@ public class MainActivity extends AppCompatActivity implements FragmentDrawer.Fr
                 getSupportFragmentManager().findFragmentById(R.id.fragment_navigation_drawer);
         drawerFragment.setUp(R.id.fragment_navigation_drawer, (DrawerLayout) findViewById(R.id.drawer_layout), mToolbar);
         drawerFragment.setDrawerListener(this);
+        loadFragment(new InternalStorageFragment(), getResources().getString(R.string.title_home));
 
     }
 
@@ -60,13 +52,18 @@ public class MainActivity extends AppCompatActivity implements FragmentDrawer.Fr
             default:
                 break;
         }
+
         if (fragment != null) {
-            FragmentManager fragmentManager = getSupportFragmentManager();
-            FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
-            fragmentTransaction.replace(R.id.container_body, fragment);
-            fragmentTransaction.commit();
-            // set the toolbar title
-            getSupportActionBar().setTitle(title);
+            loadFragment(fragment,title);
         }
+    }
+
+    private void loadFragment(Fragment fragment, String title) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.container_body, fragment);
+        fragmentTransaction.commit();
+        // set the toolbar title
+        getSupportActionBar().setTitle(title);
     }
 }
