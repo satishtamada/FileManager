dd465a97062ca39870ca6bd1c4362846c0d92a21
satishtamada <satish@confianza.co.in>
2015-12-31 10:06:31 +0530

    added files to master

--- app/src/main/java/com/example/satish/filemanager/activity/FolderItemActivity.java
+++
@@ -1,9 +0,0 @@
-package com.example.satish.filemanager.activity;
-
-import android.support.v7.app.AppCompatActivity;
-
-/**
- * Created by Satish on 05-12-2015.
- */
-public class FolderItemActivity extends AppCompatActivity {
-}
