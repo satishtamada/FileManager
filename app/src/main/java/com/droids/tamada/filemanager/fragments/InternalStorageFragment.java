e3cab65baf9c9346e26995c43c6091e1e6bbd84d
tamadasatish <tamadasatish@gmail.com>
2016-10-21 00:00:40 +0530

    updated

--- app/src/main/java/com/droids/tamada/filemanager/fragments/InternalStorageFragment.java
+++ app/src/main/java/com/droids/tamada/filemanager/fragments/InternalStorageFragment.java
@@ -2,6 +2,7 @@ package com.droids.tamada.filemanager.fragments;
 
 import android.app.Dialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.media.MediaPlayer;
@@ -14,6 +15,7 @@ import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.GestureDetector;
+import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.MotionEvent;
 import android.view.View;
@@ -73,6 +75,7 @@ public class InternalStorageFragment extends Fragment implements MainActivity.Bu
     private RelativeLayout footerLayout;
     private TextView lblFilePath;
     private ArrayList<String> arrayListFilePaths;
+    private ToggleButton toggleButtonCheck;
 
     public InternalStorageFragment() {
         // Required empty public constructor
@@ -115,7 +118,6 @@ public class InternalStorageFragment extends Fragment implements MainActivity.Bu
         recyclerView.setHasFixedSize(true);
         RecyclerView.LayoutManager mLayoutManager = new LinearLayoutManager(AppController.getInstance().getApplicationContext());
         recyclerView.setLayoutManager(mLayoutManager);
-        recyclerView.addItemDecoration(new DividerItemDecoration(AppController.getInstance().getApplicationContext(), LinearLayoutManager.VERTICAL));
         recyclerView.setItemAnimator(new DefaultItemAnimator());
         recyclerView.setAdapter(internalStorageListAdapter);
         arrayListFilePaths.add(rootPath);
@@ -123,63 +125,81 @@ public class InternalStorageFragment extends Fragment implements MainActivity.Bu
         recyclerView.addOnItemTouchListener(new RecyclerTouchListener(AppController.getInstance().getApplicationContext(), recyclerView, new ClickListener() {
             @Override
             public void onClick(View view, int position) {
+                if (footerLayout.getVisibility() != View.GONE) {
+                    Animation topToBottom = AnimationUtils.loadAnimation(AppController.getInstance().getApplicationContext(),
+                            R.anim.top_bottom);
+                    footerLayout.startAnimation(topToBottom);
+                    footerLayout.setVisibility(View.GONE);
+                }
                 InternalStorageFilesModel internalStorageFilesModel = internalStorageFilesModelArrayList.get(position);
-                fileExtension = internalStorageFilesModel.getFileName().substring(internalStorageFilesModel.getFileName().lastIndexOf(".") + 1);//file extension (.mp3,.png,.pdf)
-                File file = new File(internalStorageFilesModel.getFilePath());//get the selected item path
-                if (file.isDirectory()) {//check if selected item is directory
-                    if (file.canRead()) {//if directory is readable
-                        internalStorageFilesModelArrayList.clear();
-                        arrayListFilePaths.add(internalStorageFilesModel.getFilePath());
-                        getFilesList(internalStorageFilesModel.getFilePath());
-                        internalStorageListAdapter.notifyDataSetChanged();
-                    } else {//Toast to your not openable type
-                        Toast.makeText(AppController.getInstance().getApplicationContext(), "Folder can't be read!", Toast.LENGTH_SHORT).show();
+                if (!internalStorageFilesModel.isSelected()) {//if file not selected then open file.
+                    fileExtension = internalStorageFilesModel.getFileName().substring(internalStorageFilesModel.getFileName().lastIndexOf(".") + 1);//file extension (.mp3,.png,.pdf)
+                    File file = new File(internalStorageFilesModel.getFilePath());//get the selected item path
+                    if (file.isDirectory()) {//check if selected item is directory
+                        if (file.canRead()) {//if directory is readable
+                            internalStorageFilesModelArrayList.clear();
+                            arrayListFilePaths.add(internalStorageFilesModel.getFilePath());
+                            getFilesList(internalStorageFilesModel.getFilePath());
+                            internalStorageListAdapter.notifyDataSetChanged();
+                        } else {//Toast to your not openable type
+                            Toast.makeText(AppController.getInstance().getApplicationContext(), "Folder can't be read!", Toast.LENGTH_SHORT).show();
+                        }
+                        //if file is not directory open a application for file type
+                    } else if (fileExtension.equals("png") || fileExtension.equals("jpeg") || fileExtension.equals("jpg")) {
+                        Intent imageIntent = new Intent(getActivity().getApplicationContext(), ImageViewActivity.class);
+                        imageIntent.putExtra("imagePath", internalStorageFilesModel.getFilePath());
+                        imageIntent.putExtra("imageName", internalStorageFilesModel.getFileName());
+                        getActivity().startActivity(imageIntent);
+                    } else if (fileExtension.equals("mp3")) {
+                        showAudioPlayer(internalStorageFilesModel.getFileName(), internalStorageFilesModel.getFilePath());
+                    } else if (fileExtension.equals("txt") || fileExtension.equals("html") || fileExtension.equals("xml")) {
+                        Intent txtIntent = new Intent(getActivity().getApplicationContext(), TextFileViewActivity.class);
+                        txtIntent.putExtra("filePath", internalStorageFilesModel.getFilePath());
+                        txtIntent.putExtra("fileName", internalStorageFilesModel.getFileName());
+                        getActivity().startActivity(txtIntent);
+                    } else if (fileExtension.equals("zip") || fileExtension.equals("rar")) {
+                        //TODO handle zip file
+                    } else if (fileExtension.equals("pdf")) {
+                        File pdfFile = new File(internalStorageFilesModel.getFilePath());
+                        PackageManager packageManager = getActivity().getPackageManager();
+                        Intent testIntent = new Intent(Intent.ACTION_VIEW);
+                        testIntent.setType("application/pdf");
+                        List list = packageManager.queryIntentActivities(testIntent, PackageManager.MATCH_DEFAULT_ONLY);
+                        if (list.size() > 0 && pdfFile.isFile()) {
+                            Intent intent = new Intent();
+                            intent.setAction(Intent.ACTION_VIEW);
+                            Uri uri = Uri.fromFile(pdfFile);
+                            intent.setDataAndType(uri, "application/pdf");
+                            startActivity(intent);
+                        } else {
+                            Toast.makeText(getActivity().getApplicationContext(), "There is no app to handle this type of file", Toast.LENGTH_SHORT).show();
+                        }
+                    } else if (fileExtension.equals("mp4") || fileExtension.equals("3gp") || fileExtension.equals("wmv")) {
+                        Uri fileUri = Uri.fromFile(new File(internalStorageFilesModel.getFileName()));
+                        Intent intent = new Intent(android.content.Intent.ACTION_VIEW);
+                        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                        intent.setDataAndType(fileUri, "video/mp4");
+                        getActivity().startActivity(intent);
                     }
-                    //if file is not directory open a application for file type
-                } else if (fileExtension.equals("png") || fileExtension.equals("jpeg") || fileExtension.equals("jpg")) {
-                    Intent imageIntent = new Intent(getActivity().getApplicationContext(), ImageViewActivity.class);
-                    imageIntent.putExtra("imagePath", internalStorageFilesModel.getFilePath());
-                    imageIntent.putExtra("imageName", internalStorageFilesModel.getFileName());
-                    getActivity().startActivity(imageIntent);
-                } else if (fileExtension.equals("mp3")) {
-                    showAudioPlayer(internalStorageFilesModel.getFileName(), internalStorageFilesModel.getFilePath());
-                } else if (fileExtension.equals("txt") || fileExtension.equals("html") || fileExtension.equals("xml")) {
-                    Intent txtIntent = new Intent(getActivity().getApplicationContext(), TextFileViewActivity.class);
-                    txtIntent.putExtra("filePath", internalStorageFilesModel.getFilePath());
-                    txtIntent.putExtra("fileName", internalStorageFilesModel.getFileName());
-                    getActivity().startActivity(txtIntent);
-                } else if (fileExtension.equals("zip") || fileExtension.equals("rar")) {
-                    //TODO handle zip file
-                } else if (fileExtension.equals("pdf")) {
-                    File pdfFile = new File(internalStorageFilesModel.getFilePath());
-                    PackageManager packageManager = getActivity().getPackageManager();
-                    Intent testIntent = new Intent(Intent.ACTION_VIEW);
-                    testIntent.setType("application/pdf");
-                    List list = packageManager.queryIntentActivities(testIntent, PackageManager.MATCH_DEFAULT_ONLY);
-                    if (list.size() > 0 && pdfFile.isFile()) {
-                        Intent intent = new Intent();
-                        intent.setAction(Intent.ACTION_VIEW);
-                        Uri uri = Uri.fromFile(pdfFile);
-                        intent.setDataAndType(uri, "application/pdf");
-                        startActivity(intent);
-                    } else {
-                        Toast.makeText(getActivity().getApplicationContext(), "There is no app to handle this type of file", Toast.LENGTH_SHORT).show();
-                    }
-                } else if (fileExtension.equals("mp4") || fileExtension.equals("3gp") || fileExtension.equals("wmv")) {
-                    Uri fileUri = Uri.fromFile(new File(internalStorageFilesModel.getFileName()));
-                    Intent intent = new Intent(android.content.Intent.ACTION_VIEW);
-                    intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-                    intent.setDataAndType(fileUri, "video/mp4");
-                    getActivity().startActivity(intent);
+                } else {
+                    toggleButtonCheck = (ToggleButton) view.findViewById(R.id.id_check);
+                    toggleButtonCheck.setChecked(false);
+                    internalStorageFilesModel.setSelected(false);
                 }
             }
 
             @Override
             public void onLongClick(View view, int position) {
-                Animation bottomToTop = AnimationUtils.loadAnimation(AppController.getInstance().getApplicationContext(),
-                        R.anim.bottom_top);
-                footerLayout.startAnimation(bottomToTop);
-                footerLayout.setVisibility(View.VISIBLE);
+                if (footerLayout.getVisibility() != View.VISIBLE) {
+                    Animation bottomToTop = AnimationUtils.loadAnimation(AppController.getInstance().getApplicationContext(),
+                            R.anim.bottom_top);
+                    footerLayout.startAnimation(bottomToTop);
+                    footerLayout.setVisibility(View.VISIBLE);
+                }
+                toggleButtonCheck = (ToggleButton) view.findViewById(R.id.id_check);
+                toggleButtonCheck.setChecked(true);
+                InternalStorageFilesModel internalStorageFilesModel = internalStorageFilesModelArrayList.get(position);
+                internalStorageFilesModel.setSelected(true);
             }
         }));
         return view;
@@ -224,6 +244,20 @@ public class InternalStorageFragment extends Fragment implements MainActivity.Bu
                 }
             }
         });
+        audioPlayerDialog.setOnKeyListener(new Dialog.OnKeyListener() {
+
+            @Override
+            public boolean onKey(DialogInterface arg0, int keyCode,
+                                 KeyEvent event) {
+                // TODO Auto-generated method stub
+                if (keyCode == KeyEvent.KEYCODE_BACK) {
+                    mediaPlayer.stop();
+                    mediaPlayer.reset();
+                    audioPlayerDialog.dismiss();
+                }
+                return true;
+            }
+        });
     }
 
     private void getFilesList(String filePath) {
@@ -249,7 +283,13 @@ public class InternalStorageFragment extends Fragment implements MainActivity.Bu
 
     @Override
     public void onButtonBackPressed(int navItemIndex) {
-        if(navItemIndex==0){
+        if (footerLayout.getVisibility() != View.GONE) {
+            Animation topToBottom = AnimationUtils.loadAnimation(AppController.getInstance().getApplicationContext(),
+                    R.anim.top_bottom);
+            footerLayout.startAnimation(topToBottom);
+            footerLayout.setVisibility(View.GONE);
+        }
+        if (navItemIndex == 0) {
             if (arrayListFilePaths.size() == 1) {
                 Toast.makeText(AppController.getInstance().getApplicationContext(), "back pre", Toast.LENGTH_SHORT).show();
             } else {
@@ -263,10 +303,38 @@ public class InternalStorageFragment extends Fragment implements MainActivity.Bu
         }
     }
 
+    public void onButtonPressed(Uri uri) {
+        if (mListener != null) {
+            mListener.onFragmentInteraction(uri);
+        }
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        /*if (context instanceof OnFragmentInteractionListener) {
+            mListener = (OnFragmentInteractionListener) context;
+        } else {
+            throw new RuntimeException(context.toString()
+                    + " must implement OnFragmentInteractionListener");
+        }*/
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        mListener = null;
+    }
+
     public interface ClickListener {
         void onClick(View view, int position);
 
         void onLongClick(View view, int position);
+    }
+
+    public interface OnFragmentInteractionListener {
+        // TODO: Update argument type and name
+        void onFragmentInteraction(Uri uri);
     }
 
     public static class RecyclerTouchListener implements RecyclerView.OnItemTouchListener {
@@ -308,34 +376,6 @@ public class InternalStorageFragment extends Fragment implements MainActivity.Bu
         public void onRequestDisallowInterceptTouchEvent(boolean disallowIntercept) {
 
         }
-    }
-
-    public void onButtonPressed(Uri uri) {
-        if (mListener != null) {
-            mListener.onFragmentInteraction(uri);
-        }
-    }
-
-    @Override
-    public void onAttach(Context context) {
-        super.onAttach(context);
-        /*if (context instanceof OnFragmentInteractionListener) {
-            mListener = (OnFragmentInteractionListener) context;
-        } else {
-            throw new RuntimeException(context.toString()
-                    + " must implement OnFragmentInteractionListener");
-        }*/
-    }
-
-    @Override
-    public void onDetach() {
-        super.onDetach();
-        mListener = null;
-    }
-
-    public interface OnFragmentInteractionListener {
-        // TODO: Update argument type and name
-        void onFragmentInteraction(Uri uri);
     }
 
 
